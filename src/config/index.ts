42e3a4a2673bb395420721656a80fdc3f82c49d4
Aldo Abril <aldoabril@gmail.com>
2023-01-04 17:49:21 -0500

    ruta externa

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,6 +1,7 @@
 import app from "../../env.json"
 export const config = {
   idApp:  app.ID_APP,
-  urlBase : "http://scn20rest:5090",
+  // urlBase : "http://scn20rest:5090",
+  urlBase : "http://www.ensa.com.pe:5090",
   timeOut: 5000
 }
